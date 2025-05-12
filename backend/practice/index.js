b8777c1d738da1ab9b1bc5e46712f5ee56786c14
neelimaBonumaddi <neelimabonumaddi@gmail.com>
2025-05-12 23:49:41 +0530

    added sqlite

--- backend/practice/index.js
+++ backend/practice/index.js
@@ -2,6 +2,40 @@ const greeting = (name) => {
     console.log( `hello ${name}`);
 };
 
+const addDays = require("date-fns/addDays");
+const result = addDays(new Date(2021, 0, 11), 10);
+
+console.log(result);
+
 greeting("neelima");
 greeting("yaswanth");
 greeting("vamsi");
+
+
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+
+const dbPath = path.join(__dirname, "goodreads.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
